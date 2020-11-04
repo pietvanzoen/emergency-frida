7e063aaa2ec720b0955f75f685c887301e3cfcfa
pietvanzoen <hi@pietvanzoen.com>
2020-11-04 13:41:28 +0100

    initial setup

---
+++ scripts/optimize.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+const glob = require('glob');
+const sharp = require('sharp');
+const Promise = require('bluebird');
+const { extname, join, basename } = require('path');
+const crypto = require('crypto');
+
+
+const SOURCE_DIR = './originals';
+const OUT_DIR = './images';
+
+const successList = [];
+const errorList = {};
+
+glob(`${SOURCE_DIR}/*`, function (er, files) {
+  Promise.resolve(files)
+    .mapSeries((file) => resize(file, join(OUT_DIR, makefilename(file))))
+    .then(() => Promise.fromCallback(function(cb) {
+        fs.writeFile('resize.log', JSON.stringify(errorList, null, 2) , cb);
+      })
+        .catch(function(err) {
+          errorList['write_log_to_file_error'] = err;
+        })
+    )
+    .then(function() {
+      console.log('------------------------------');
+      console.log('TOTAL:', files.length);
+      console.log('OKAY:', successList.length);
+      console.log('FAIL:', Object.keys(errorList).length);
+    });
+});
+
+function resize(sourceFile, destFile) {
+  return sharp(sourceFile)
+    .resize(1200, 1200)
+		.rotate()
+    .toFormat('jpeg', { progressive: true, quality: 50 })
+    .toBuffer()
+    .then(buffer => Promise.fromCallback(cb => fs.writeFile(destFile, buffer, cb)))
+    .then(function() {
+      successList.push(sourceFile);
+      console.log('Okay:', sourceFile);
+    })
+    .catch(function(err) {
+      console.log('Fail:', sourceFile, '\n', err);
+      errorList[sourceFile] = { message: err.message, fileName: err.fileName, lineNumber: err.lineNumber };
+    });
+}
+
+function makefilename(filepath) {
+  const hash = crypto.createHash('md5').update(basename(filepath)).digest('hex');
+  return hash.substring(0, 10) + extname(filepath);
+}
---
+++ server.js
@@ -0,0 +1,72 @@
+const { readdirSync, readFileSync } = require('fs');
+const fastify = require('fastify');
+const path = require('path');
+
+
+const fridas = readdirSync('./public/images');
+const view = readFileSync('./view.html', 'utf-8');
+const fourohfour = readFileSync('./404.html', 'utf-8');
+
+
+const server = fastify();
+
+server.register(require('fastify-static'), {
+  root: path.join(__dirname, 'public'),
+  prefix: '/public/',
+})
+
+server.get('/', async (request, reply) => {
+  const frida = sample(fridas);
+  reply.type('text/html');
+  return template(frida);
+})
+
+server.get('/-/:id', async (request, reply) => {
+  const frida = fridas.find(f => f.startsWith(request.params.id));
+  reply.type('text/html');
+  if (!frida) {
+    reply.code(404);
+    return fourohfour;
+  }
+  return template(frida);
+})
+
+server.get('*', async (request, reply) => {
+  reply.type('text/html');
+  reply.code(404);
+  return fourohfour;
+});
+
+server.listen(8080, (err, address) => {
+ if(err) {
+   console.error(err)
+   process.exit(1)
+ }
+ console.log(`Server listening at ${address}`)
+})
+
+// server.get('/ping', async (request, reply) => {
+//  return 'pong\n'
+// })
+
+// server.listen(8080, (err, address) => {
+//  if(err) {
+//    console.error(err)
+//    process.exit(1)
+//  }
+//  console.log(`Server listening at ${address}`)
+// })
+//
+function template(frida) {
+  return view
+    .replace('{{src}}', path.join('/public/images', frida))
+    .replace('{{permalink}}', permalink(frida));
+}
+
+function sample(items) {
+   return items[Math.floor(Math.random() * items.length)];
+}
+
+function permalink(file) {
+  return path.join('/-/', path.parse(file).name);
+}
